3a0330d92ae1d6fbeb10eb666d9a7edb186f30df
Diego <diego3209e@gmail.com>
2020-12-05 11:07:16 -0600

    Catalogs

---
+++ catalogos.sql
@@ -0,0 +1,30 @@
+INSERT INTO ROL VALUES(null, 'Alumno', '');
+INSERT INTO ROL VALUES(null, 'Docente', '');
+INSERT INTO ROL VALUES(null, 'Administrativo', '');
+
+INSERT INTO CARRERA VALUES(null, 'Ingeniería Civil', '');
+INSERT INTO CARRERA VALUES(null, 'Ingeniería Industrial', '');
+INSERT INTO CARRERA VALUES(null, 'Ingeniería Mecatrónica', '');
+INSERT INTO CARRERA VALUES(null, 'Ingeniería Cibernética', '');
+
+INSERT INTO SEMESTRE VALUES(null, 'Primer semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Segundo semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Tercero semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Cuarto semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Quinto semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Sexto semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Septimo semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Octavo semestre', '');
+INSERT INTO SEMESTRE VALUES(null, 'Noveno semestre', '');
+
+INSERT INTO GRUPO(null, '101', '');
+INSERT INTO GRUPO(null, '102', '');
+INSERT INTO GRUPO(null, '201', '');
+INSERT INTO GRUPO(null, '202', '');
+INSERT INTO GRUPO(null, '301', '');
+INSERT INTO GRUPO(null, '401', '');
+INSERT INTO GRUPO(null, '501', '');
+INSERT INTO GRUPO(null, '601', '');
+INSERT INTO GRUPO(null, '701', '');
+INSERT INTO GRUPO(null, '801', '');
+INSERT INTO GRUPO(null, '901', '');
