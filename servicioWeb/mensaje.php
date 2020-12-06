d732628357091fe234b8962602ee876f818fbae3
Diego <diego3209e@gmail.com>
2020-12-06 11:21:06 -0600

    Envio de mensajes

--- servicioWeb/mensaje.php
+++ servicioWeb/mensaje.php
@@ -6,7 +6,7 @@ if(isset($_REQUEST['op'])) {
             listarMensajes();
         break;
     
-        case 'carrera':
+        case 'crear':
             crearMensaje();
         break;
     
@@ -19,6 +19,37 @@ if(isset($_REQUEST['op'])) {
 else {
     http_response_code(400);
     echo json_encode([400, 'Bad Request']);
+}
+
+function crearMensaje() {
+    if (isset($_REQUEST['id']) && isset($_REQUEST['mensaje'])) {
+        $datos = array();
+        $id = $_REQUEST['id'];
+        $mensaje = $_REQUEST['mensaje'];
+        $rol = -1;
+        $carrera = -1;
+        $grupo = -1;
+        $semestre = -1;
+        if (isset($_REQUEST['rol'])) $rol = $_REQUEST['rol'];
+        if (isset($_REQUEST['carrera'])) $carrera = $_REQUEST['carrera'];
+        if (isset($_REQUEST['grupo'])) $grupo = $_REQUEST['grupo'];
+        if (isset($_REQUEST['semestre'])) $semestre = $_REQUEST['semestre'];
+
+        include_once 'conexion.php';
+        if ($conn = mysqli_connect($server, $dbuser, $dbpass, $bd)) {
+            $renglon = mysqli_query($conn, "CALL stpNuevoMensaje('$mensaje', $id, $rol, $carrera, $grupo, $semestre)");
+            while ($resultado = mysqli_fetch_assoc($renglon)) {
+                $datos["RES"] = $resultado['RES'];
+            }
+            mysqli_close($conn);
+        }
+        echo json_encode($datos);
+
+    }
+    else {
+        http_response_code(400);
+        echo json_encode([400, 'Bad Request']);
+    }
 }
 
 function listarMensajes() {
