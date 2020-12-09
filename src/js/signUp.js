dbaf5814330b4400b5f21a1654843087e6bbd344
Ykurgan <volalol1@hotmail.com>
2020-12-08 20:52:56 -0600

    js registro

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', signUpF);
 let r = 0;
 function signUpF(){
     const frmSignUp = document.getElementById('signUp');
+    let email = document.getElementById('email');
     frmSignUp.addEventListener('submit', function(e){
         e.preventDefault();
         let firstName = frmSignUp.getElementById('fName');
@@ -10,19 +11,42 @@ function signUpF(){
         let pass = frmSignUp.getElementById('pass');
         let role;// = frmSignUp.getElementById('role');
         let phone = frmSignUp.getElementById('phone');
-        let email = frmSignUp.getElementById('email');
+
+        let career = frmSignUp.getElementById('career');
+        let semester = frmSignUp.getElementById('semester');
+        let group = frmSignUp.getElementById('group');
+        email = frmSignUp.getElementById('email');
         if(r==1){
             role = 2;
+            let params = `op=registrar&&usuario=${user.value}&&contra=${pass.value}&&nombre=${firstName.value}&&ap=${lastName.value}&&rol=${role.value}&&tel=${phone.value}&&email=${email.value}`;
         }
         else{
             role = 1;
+            let params = `op=registrar&&usuario=${user.value}&&contra=${pass.value}&&nombre=${firstName.value}&&ap=${lastName.value}&&rol=${role.value}&&tel=${phone.value}&&email=${email.value} &&carrera=${career.value}&&grupo=${group.value}&&semestre=${semester.value}`;
         }
         
-        let params = `op=registrar&&usuario=${user.value}&&contra=${pass.value}&&nombre=${firstName.value}&&ap=${lastName.value}&&rol=${role.value}&&tel=${phone.value}&&email=${email.value}`;
+        
         login(params);
         //const data = new FormData(params);
     });
-    frmSignUp.addEventListener('blur',document.getElementById('email'), disableInputs());
+    console.log(email.value);
+    email.addEventListener('focusout', function(){
+        console.log(email.value);
+    if(email.value.includes(".edu.")){
+        r = 1;
+        console.log("Desactivar");
+        document.getElementById('career').setAttribute("disabled", "true");
+        document.getElementById('semester').setAttribute("disabled", "true");
+        document.getElementById('group').setAttribute("disabled", "true");
+    }
+    else{
+        r = 0;
+        console.log("Activar");
+        document.getElementById('career').removeAttribute("disabled");
+        document.getElementById('semester').removeAttribute("disabled");
+        document.getElementById('group').removeAttribute("disabled");
+    }
+    });
     let login = (data)=>{
         fetch('../../servicioWeb/contacto.php?'+data, {
             method:'GET'
@@ -32,16 +56,4 @@ function signUpF(){
     let verify = (res)=>{
         console.log(res);
     }
-}
-
-function disableInputs(){
-    if(email.includes(".edu.")){
-        r = 1;
-        document.getElementById('carrera').disabled = true;
-    }
-    else{
-        r = 0;
-        document.getElementById('carrera').disabled = false;
-    }
-
 }
